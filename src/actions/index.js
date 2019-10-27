1ad409ef49b66623c0aa9f2c6dbaaa2bb3da4009
YWhalen <whalenyukiko@gmail.com>
2019-10-27 19:57:18 +0900

    hide API key

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,10 @@ import axios from 'axios' // API取得用
 // 非同期取得操作
 export const getMovies = (year, category) => {
     return (dispatch) => {
+      var mykey = config.MY_KEY;
       dispatch(getMoviesRequest());
 
-      return axios.get('https://api.themoviedb.org/3/discover/movie?primary_release_year='  + year + '&api_key=1b5adf76a72a13bad99b8fc0c68cb085&sort_by=' + category + '.desc&language=en-US&certification_country=US&external_source=twitter_id')
+      return axios.get('https://api.themoviedb.org/3/discover/movie?primary_release_year='  + year + '&api_key=' + mykey +'&sort_by=' + category + '.desc&language=en-US&certification_country=US&external_source=twitter_id')
         .then(response => dispatch(getMoviesSuccess(response.data.results)))
         .catch(error => dispatch(getMoviesFailure(error)))
     };
